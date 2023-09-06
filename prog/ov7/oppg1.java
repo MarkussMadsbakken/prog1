2c07d5a7a60c61a20de6545e1faee54acb3d6349
Markus <markussmadsbakken@gmail.com>
2023-09-06 11:48:15 +0200

    mer oving

--- prog/ov7/oppg1.java
+++ prog/ov7/oppg1.java
@@ -2,6 +2,52 @@ package prog.ov7;
 
 public class oppg1 {
     public static void main(String[] args) {
+        nyString a = new nyString("     Hei     øaokhfakøjshdfkjøashfd mn nn nnnnn   znxcb");
+        System.out.println(a.forkort());
+        System.out.println(a.fjern("H"));
         
+    }
+
+    public static class nyString{
+        String string;
+
+        public nyString(String _string){
+            string = _string;
+        }
+
+        public String forkort(){
+            String res = "";
+            int start = 0;
+
+            for(int i = 0; i < string.length(); i++){
+                if(string.charAt(i) != " ".charAt(0)){
+                    res += string.charAt(i);
+                    start = i;
+                    System.out.println(i);
+                    break;
+                }
+            }
+    
+            for(int i = start; i < string.length(); i++){
+                if(string.charAt(i) == " ".charAt(0) && string.charAt(i+1) != " ".charAt(0)){
+                    res += string.charAt(i+1);
+                }
+            }
+
+            return res;
+        }
+
+        public String fjern(String fjern){
+            String res = "";
+
+            for(int i = 0; i < string.length(); i++){
+                if(string.charAt(i) != fjern.charAt(0)){
+                    res += string.charAt(i);
+                }
+            }
+
+            return res;
+        }
+
     }
 }
---
+++ prog/ov7/oppg2.java
@@ -0,0 +1,8 @@
+package prog.ov7;
+
+public class oppg2 {
+    public static void main(String[] args){
+        
+    }
+    
+}
